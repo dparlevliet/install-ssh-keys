2774689eef64b16376f6d093b67755727cfc075c
Mr. Meeseeks <1628264+dparlevliet@users.noreply.github.com>
2019-07-18 18:40:34 +1000

    fixes to generate file clean and correct

--- download.sh
+++ download.sh
@@ -8,10 +8,11 @@ if ! [[ -f ~/.ssh/authorized_keys ]]; then
   chmod 664 ~/.ssh/authorized_keys
 fi
 
-keys=$(curl https://api.github.com/users/$USERNAME/keys | grep -o -E "ssh-\w+\s+[^\"]+")
+keys=$(curl https://api.github.com/users/$USERNAME/keys | grep "ssh-" | sed 's/"key": "//g' | sed 's/"//g' | sed -E 's/    //g')
 
-for key in $keys; do
+echo "" > ~/.ssh/authorized_keys
+while read -r key; do
   echo $key
   grep -q "$key" ~/.ssh/authorized_keys || echo "$key" >> ~/.ssh/authorized_keys
-  echo "" >> ~/.ssh/authorized_keys # fix for ssh read bug
-done
+done <<< $keys
+echo "" >> ~/.ssh/authorized_keys # fix for ssh read bug
